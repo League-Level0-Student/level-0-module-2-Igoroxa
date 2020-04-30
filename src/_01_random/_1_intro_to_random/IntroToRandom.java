efabc7a3c1990a46242cba85d48fd0d997eaf100
root <root@a7a610d7ea49>
2020-04-30 23:56:55 +0000

    Magic 8 ball

--- src/_01_random/_1_intro_to_random/IntroToRandom.java
+++ src/_01_random/_1_intro_to_random/IntroToRandom.java
@@ -7,8 +7,8 @@ import java.util.Random;
 
 public class IntroToRandom {
 	public static void main(String[] args) {
-		
-		int num = 0;
+		Random ran = new Random();
+		int num = ran.nextInt((88 - -222)+1) + -222;
 		
 		//3   Now to make num random. 
 		//3a. Create an object of the Random class, Hint: Random ran = new Random()
@@ -19,11 +19,11 @@ public class IntroToRandom {
 		//4. Now limit the value of num to be between 0 and 100
 
 		//5. Now limit the value of num to be between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
-
+		
 		//6. Challenge: Limit the value of num to be between -222 and 88
 
 		//1. Print out the value of num
-		
+		System.out.println(num);
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?
 	}
--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -12,14 +12,29 @@ public class Validation {
 		
 		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
+		
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		for (int i = 0; i < 11; i++) {
+			int randomNumber = randomMaker.nextInt(5);
+			
+		
+if (randomNumber == 0){
+	JOptionPane.showMessageDialog(null, "I like your shoes!");}
+if (randomNumber == 1){
+		JOptionPane.showMessageDialog(null, "I like you shirt");}
+if (randomNumber == 2){
+			JOptionPane.showMessageDialog(null, "You have a good sense of humor!");}
+if (randomNumber == 3){
+				JOptionPane.showMessageDialog(null, "I don't like your shoes!");}
+if (randomNumber == 4){
+					JOptionPane.showMessageDialog(null, "Your are smart!");}
+		}
+}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
 	}
-}
+
